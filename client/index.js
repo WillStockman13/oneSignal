e5447d25eef38c2d7529c6430dbfc7c8a2403775
Will Stockman <WillStockman13@gmail.com>
2017-02-08 14:50:31 -0800

    fixed

--- client/index.js
+++ client/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 import App from './components/App/App.js';
-import Home from './components/Home/Home.js';
+import HOme from './components/Home/Home.js';
 
 
 ReactDOM.render((
   <div>
     <Router history={browserHistory} >
       <Route path='/' component={App}>
-        <IndexRoute component={Home} />
+        <IndexRoute component={HOme} />
       </Route>
     </Router>
   </div>
